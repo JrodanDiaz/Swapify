a511bf9dd42114de3fb7e4f15b5df7a7ebf15a27
Jordan <jordancdiaz04@gmail.com>
2024-06-01 13:48:15 -0400

    add temp tailwind color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "main-red": "#ECE8EF",
+        "main-pink": "#D34F73",
+        "main-black": "#040F16",
+        "main-lightblack": "#0A100D",
+      },
     },
   },
   plugins: [],

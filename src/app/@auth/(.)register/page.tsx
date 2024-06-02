cc2206fcfca620b8e3510a8dede1433a671480ea
Jordan <jordancdiaz04@gmail.com>
2024-06-02 16:32:09 -0400

    add Delay component so the loading skeleton can be shown off

--- src/app/@auth/(.)register/page.tsx
+++ src/app/@auth/(.)register/page.tsx
@@ -1,11 +1,15 @@
 import { Modal } from "@/app/components/Modal";
 import SignupForm from "@/app/components/auth/SignupForm";
+import Delay from "@/app/components/Delay";
+import RegisterFormSkeleton from "@/app/components/RegisterFormSkeleton";
 
 export default function RegisterModal() {
   return (
     <>
       <Modal>
-        <SignupForm />
+        <Delay placeholder={<RegisterFormSkeleton />} delay={500}>
+          <SignupForm />
+        </Delay>
       </Modal>
     </>
   );

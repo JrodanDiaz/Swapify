0f2223a1791ccac859f6b358df14900db3a97282
Jordan <jordancdiaz04@gmail.com>
2024-06-02 04:17:52 -0400

    align form with container-div

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -20,39 +20,43 @@ export default function RegisterForm({
   };
   return (
     <>
-      <div className=" relative h-[420px] max-w-sm w-full p-4 bg-white rounded-md shadow-lg border-black border-[1px] flex flex-col items-center justify-evenly">
-        <button
-          onClick={closeModal}
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
-        >
-          Exit
-        </button>
-        <h2 className=" font-bold text-3xl animate-fadeIn">{formData.title}</h2>
-        <div className="flex flex-col animate-fadeIn">
-          {formItems.map((item) => (
-            <>
-              <label htmlFor={item.id} className="font-semibold">
-                {item.label}
-              </label>
-              <input
-                type={item.type}
-                id={item.id}
-                name={item.id}
-                placeholder={item.placeholder}
-                className="border-black border-[1px] px-2 py-1 mb-3"
-              />
-            </>
-          ))}
-        </div>
-        <div className="animate-fadeIn">
+      <div className=" relative h-[420px] max-w-sm w-full p-1 bg-white rounded-md shadow-lg border-black border-[1px] flex flex-col items-center justify-evenly">
+        <div className="border-black-600 w-5/6 h-5/6 flex flex-col  justify-evenly animate-fadeIn">
           <button
-            type="submit"
-            className=" bg-main-pink text-white px-4 py-2 rounded-sm"
+            onClick={closeModal}
+            className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
           >
-            {formData.button}
+            Exit
           </button>
+          <h2 className=" font-bold text-3xl animate-fadeIn">
+            {formData.title}
+          </h2>
+          <div className="flex flex-col w-full animate-fadeIn">
+            {formItems.map((item) => (
+              <>
+                <label htmlFor={item.id} className="font-semibold">
+                  {item.label}
+                </label>
+                <input
+                  type={item.type}
+                  id={item.id}
+                  name={item.id}
+                  placeholder={item.placeholder}
+                  className="border-black border-[1px] px-2 py-1 mb-3 w-full"
+                />
+              </>
+            ))}
+          </div>
+          <div className="animate-fadeIn w-full">
+            <button
+              type="submit"
+              className="bg-main-lightblack text-white px-4 py-2 rounded-sm w-full"
+            >
+              {formData.button}
+            </button>
+          </div>
+          {footer}
         </div>
-        {footer}
       </div>
     </>
   );

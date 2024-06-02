9edf08f24ffc17f5b8fa7c690573ef6fdc9e6a96
Jordan <jordancdiaz04@gmail.com>
2024-06-01 22:12:58 -0400

    add dummy terms of service page

---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,12 @@
+export default function TermsOfService() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center text-center">
+        <div>
+          <h1 className=" text-2xl font-semibold">Terms of Service</h1>
+          <p>erm</p>
+        </div>
+      </div>
+    </>
+  );
+}

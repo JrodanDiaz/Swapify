b12254394c818ebbccd2e976c59a3525e92c287d
Jordan <jordancdiaz04@gmail.com>
2024-06-08 10:39:46 -0400

    set required form criteria

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -7,7 +7,8 @@ import CornerButton from "./CornerButton";
 import { useFormState } from "react-dom";
 import { FormMode } from "../_types/types";
 import { RegisterInputs } from "../_types/types";
-import { useState } from "react";
+import { InputsEdited } from "../_types/types";
+import { useEffect, useState } from "react";
 
 interface RegisterFormProps {
   mode: FormMode;
@@ -35,15 +36,29 @@ const RegisterForm = ({
     username: "",
     password: "",
   });
+  const [inputEdited, setInputEdited] = useState<InputsEdited>({
+    email: false,
+    username: false,
+    password: false,
+  });
+
+  const [inputError, setInputError] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
 
   const inputsAreFilled = () => {
     const { email, username, password } = formState;
-    const formattedEmail = email.trim();
-    const formattedUsername = username.trim();
-    const formattedPassword = password.trim();
-    if (mode === "login") return formattedUsername && formattedPassword;
-    if (mode === "register")
-      return formattedEmail && formattedUsername && formattedPassword;
+    if (mode === "login") return username && password;
+    if (mode === "register") return email && username && password;
+    return false;
+  };
+
+  const inputsAreValid = () => {
+    const { email, username, password } = inputError;
+    if (mode === "login") return !username && !password;
+    if (mode === "register") return !email && !username && !password;
     return false;
   };
 
@@ -51,14 +66,45 @@ const RegisterForm = ({
     const { name, value } = event.target;
     setFormState({
       ...formState,
-      [name]: value,
+      [name]: value.trim(),
+    });
+
+    setInputEdited({
+      ...inputEdited,
+      [name]: true,
     });
   };
 
+  useEffect(() => {
+    const validateInput = () => {
+      const errors = {
+        email: "",
+        username: "",
+        password: "",
+      };
+
+      if (formState.username.length > 0 && formState.username.length < 4) {
+        errors.username = "Username must be at least 4 characters";
+      } else {
+        errors.username = "";
+      }
+
+      if (formState.password.length > 0 && formState.password.length < 6) {
+        errors.password = "Password must be atleast 6 characters";
+      } else {
+        errors.password = "";
+      }
+
+      setInputError(errors);
+    };
+
+    validateInput();
+  }, [formState]);
+
   return (
     <>
-      <div className=" relative h-[420px] max-w-sm w-full p-1 bg-white rounded-md shadow-lg border-black border-[1px] flex flex-col items-center justify-evenly">
-        <div className="border-black-600 w-5/6 h-5/6 flex flex-col justify-evenly animate-fadeIn">
+      <div className=" relative min-h-[480px] max-w-sm w-full p-1 bg-white rounded-md shadow-lg border-black border-[1px] flex flex-col items-center justify-evenly">
+        <div className="border-black-600 w-5/6 h-5/6 flex flex-col p-3 justify-evenly animate-fadeIn">
           <CornerButton
             icon="/x.svg"
             onClick={closeModal}
@@ -75,7 +121,7 @@ const RegisterForm = ({
                 <>
                   <label
                     htmlFor={item.id}
-                    className="font-semibold"
+                    className="font-semibold mt-3"
                     key={`label-${i}`}
                   >
                     {item.label}
@@ -85,19 +131,26 @@ const RegisterForm = ({
                     id={item.id}
                     name={item.id}
                     placeholder={item.placeholder}
-                    className="border-black border-[1px] px-2 py-1 mb-3 w-full"
+                    className="border-black border-[1px] px-2 py-1 mb-0 w-full"
                     onChange={handleInputChange}
                     autoComplete={item.id === "email" ? "on" : "off"}
                     key={`input-${i}`}
                   />
+                  {inputError[item.id as keyof RegisterInputs] && (
+                    <p className="text-red-600">
+                      {inputError[item.id as keyof RegisterInputs]}
+                    </p>
+                  )}
                 </>
               ))}
             </div>
-            <div className="animate-fadeIn w-full">
+            <div className="animate-fadeIn w-full mt-2">
               <button
-                type={inputsAreFilled() ? "submit" : "button"}
+                type={
+                  inputsAreFilled() && inputsAreValid() ? "submit" : "button"
+                }
                 className={`${
-                  inputsAreFilled()
+                  inputsAreFilled() && inputsAreValid()
                     ? "bg-main-lightblack text-white transition-colors duration-400"
                     : "bg-neutral-200 text-black text-opacity-20  cursor-not-allowed hover:bg-neutral-300 transition-colors duration-400"
                 } border-black border-[1px] px-4 py-2 rounded-sm w-full`}

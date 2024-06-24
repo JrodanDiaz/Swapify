3b950178691a11c9035b66f9b355081ab357aaa9
Jordan <jordancdiaz04@gmail.com>
2024-06-24 19:10:57 -0400

    /admin route now accessable without route interception

--- src/app/(routes)/@admin/(.)admin/AdminBoard.tsx
+++ src/app/(routes)/@admin/(.)admin/AdminBoard.tsx
@@ -10,7 +10,7 @@ export default function AdminBoard() {
 
   return (
     <>
-      <div className=" relative h-[420px] w-[600px] p-1 bg-white rounded-md shadow-lg border-black border-[1px] flex flex-col items-center justify-center">
+      <div className=" relative h-[420px] w-[600px] p-1 bg-black rounded-md shadow-lg shadow-white border-white border-[1px] flex flex-col items-center justify-center">
         <div className="border-black-600 text-center w-5/6 h-5/6 flex flex-col justify-evenly items-center animate-fadeIn">
           <CornerButton
             icon="/x.svg"
@@ -20,12 +20,6 @@ export default function AdminBoard() {
             topRight={true}
           />
           <form action={getUsersAction}>
-            <input
-              type="text"
-              name="table"
-              id="table"
-              className="border-black border-[1px] px-2 py-1 mb-3 w-full"
-            />
             <button
               type="submit"
               className="w-full bg-orange-600 border-black border-[1px] text-black rounded-sm px-4 py-2 mb-2"
